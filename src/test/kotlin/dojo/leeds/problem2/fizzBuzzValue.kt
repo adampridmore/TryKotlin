c702cd479c06df02c6de0820a264ade91b9706ee
Adam Pridmore <adam_pridmore@hotmail.com>
2017-11-03 20:05:24 +0000

    FizzBuzz with pattern matching.

--- src/test/kotlin/dojo/leeds/problem2/fizzBuzzValue.kt
+++ src/test/kotlin/dojo/leeds/problem2/fizzBuzzValue.kt
@@ -1,22 +1,12 @@
 package dojo.leeds.problem2
 
-
-fun fizzBuzzValue(i: Int, fizzDivisor : Int = 3, buzzDivisor : Int = 7): String {
-    if (i % (fizzDivisor*buzzDivisor) == 0) {
-        return "Fizz Buzz"
-    }
-
-    if (i % fizzDivisor == 0) {
-        return "Fizz"
-    }
-
-    if (i % buzzDivisor== 0) {
-        return "Buzz"
-    }
-
-    return i.toString()
+fun fizzBuzzValue(i: Int,fizzDivisor : Int = 3, buzzDivisor : Int = 5 ) : String = when {
+    i % (fizzDivisor*buzzDivisor) ==0 -> "Fizz Buzz"
+    i % fizzDivisor == 0 -> "Fizz"
+    i % buzzDivisor== 0 -> "Buzz"
+    else -> i.toString()
 }
 
-fun fizzBuzz(i: Int): List<String>? {
+fun fizzBuzz(i: Int): List<String> {
     return (1..i).map { it -> fizzBuzzValue(it)}
 }
